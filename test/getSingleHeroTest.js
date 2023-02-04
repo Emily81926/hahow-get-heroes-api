c1aeadb0e303fc19cb5b6916d90eef5731c119a5
Emily81926 <emily81926@gmail.com>
2023-02-05 02:07:05 +0800

    feat: add single hero data not found situation in test

--- test/getSingleHeroTest.js
+++ test/getSingleHeroTest.js
@@ -6,36 +6,41 @@ const should = chai.should();
 
 //取得「不需要」授權hero資料測試
 describe("get single hero basic request", () => {
-  //測試是否可以取得“不需授權”的單一hero資料
-  it("should get 200 and id 1 hero data", (done) => {
-    request(app)
-      .get("/heroes/1")
-      .set("Accept", "application/json")
-      .expect(200)
-      .end((err, res) => {
-        //如果有error，回傳error
-        if (err) return done(err);
-        //如果沒有error，res.body內需為物件
-        expect(res.body).to.be.an("object");
-        //取得的hero資料id為1
-        res.body["id"].should.equal("1");
-        done();
-      });
+  //可以成功取得單一hero資料
+  context("get hero data", () => {
+    it("should get 200 and id 1 hero data", (done) => {
+      request(app)
+        .get("/heroes/1")
+        .set("Accept", "application/json")
+        .expect(200)
+        .end((err, res) => {
+          //如果有error，回傳error
+          if (err) return done(err);
+          //如果沒有error，res.body內需為物件
+          expect(res.body).to.be.an("object");
+          //取得的hero資料id為1
+          res.body["id"].should.equal("1");
+          done();
+        });
+    });
   });
 
-  it("should get 200 and id 5 hero data", (done) => {
-    request(app)
-      .get("/heroes/5")
-      .set("Accept", "application/json")
-      .expect(200)
-      .end((err, res) => {
-        //如果有error，回傳error
-        if (err) return done(err);
-        //如果沒有error，res.body內需為物件
-        expect(res.body).to.be.an("object");
-        //取得的hero資料id為5
-        res.body["id"].should.equal("5");
-        done();
-      });
-  });
+  //所輸入的id找不到資料
+  context("hero data not found", () => {
+    it("should get 404 and error message", (done) => {
+      request(app)
+        .get("/heroes/10")
+        .set("Accept", "application/json")
+        .expect(404)
+        .end((err, res) => {
+          //如果有error，回傳error
+          if (err) return done(err);
+          //如果沒有error，res.body內需為物件
+          expect(res.body).to.be.an("object");
+          //取得的錯誤資訊
+          res.body["message"].should.equal("cannot find the data");
+          done();
+        })
+    })
+  })
 });
