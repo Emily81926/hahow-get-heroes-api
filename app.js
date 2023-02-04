6a15153d8539a041a338b400db6a01b6c2a2e03e
Emily81926 <emily81926@gmail.com>
2023-02-04 20:22:41 +0800

    feat: install mocha chai supertest and body-parser

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const routes = require('./routes/index')
-const app = express()
-const port = process.env.PORT
+const express = require("express");
+const routes = require("./routes/index");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT;
 
-app.use(routes)
+app.use(bodyParser.json());
+app.use(routes);
 app.listen(port, () => console.log(`This server is running on port ${port}`));
 
 module.exports = app;
