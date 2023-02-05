c141f9337cf2ead743f842eedce68ac7df27a2ca
Emily81926 <emily81926@gmail.com>
2023-02-05 15:31:44 +0800

    feat: add get authenticated single hero data test

--- test/getSingleHeroTest.js
+++ test/getSingleHeroTest.js
@@ -40,7 +40,75 @@ describe("get single hero basic request", () => {
           //取得的錯誤資訊
           res.body["message"].should.equal("cannot find the data");
           done();
-        })
-    })
-  })
+        });
+    });
+  });
+});
+
+//取得需要授權hero資料
+describe("get single hero authenticated request", () => {
+  //測試帳號密碼正確的狀況
+  context("name and password correct", () => {
+    it("should get 200 and profile", (done) => {
+      request(app)
+        .get("/heroes/1")
+        .set("Accept", "application/json")
+        .set("Content-Type", "application/json")
+        .set("Name", "hahow")
+        .set("Password", "rocks")
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body).to.be.an("object");
+          //有取得該hero資料，且包含profile的object資料
+          expect(res.body["profile"]).to.be.an("object");
+          done();
+        });
+    });
+  });
+
+  //測試帳號或密碼不正確的狀況
+  context("name or password wrong", () => {
+    it("should get 401", (done) => {
+      request(app)
+        .get("/heroes/1")
+        .set("Accept", "application/json")
+        .set("Content-Type", "application/json")
+        .set("Name", "hahow")
+        .set("Password", "rockssss")
+        .expect(401)
+        .end((err, res) => {
+          if (err) return done(err);
+          //雖然驗證失敗，但是仍可以取得hero不需授權的資料
+          expect(res.body).to.be.an("object");
+          //取得的hero資料id為1
+          res.body["id"].should.equal("1");
+          //不應該有profile資料出現
+          expect(res.body).to.not.include({ profile: {} });
+          done();
+        });
+    });
+  });
+
+  //測試輸入參數錯誤的狀況
+  context("invalid request framing", () => {
+    it("should get 400", (done) => {
+      request(app)
+        .get("/heroes/1")
+        .set("Accept", "application/json")
+        .set("Content-Type", "application/json")
+        .set("Show", "me the secret")
+        .expect(400)
+        .end((err, res) => {
+          if (err) return done(err);
+          //雖然驗證失敗，但是仍可以取得heroes不需授權的資料
+          expect(res.body).to.be.an("object");
+          //取得的hero資料id為1
+          res.body["id"].should.equal("1");
+          //不應該有profile資料出現
+          expect(res.body).to.not.include({ profile: {} });
+          done();
+        });
+    });
+  });
 });
