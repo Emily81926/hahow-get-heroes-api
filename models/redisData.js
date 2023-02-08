7abcdc6cb2b1b9e86c9cb0b67414d35f7cafc9ea
Emily81926 <emily81926@gmail.com>
2023-02-08 11:49:11 +0800

    feat: add put single hero data in redis feature in redisData.js

--- models/redisData.js
+++ models/redisData.js
@@ -7,7 +7,7 @@ promisifyAll(Redis);
 const redisData = async () => {
   // Connect to redis at 127.0.0.1 port 6379 no password.
   const redisClient = Redis.createClient();
-  //將Heroes所有資料放進redis
+  //1.將Heroes所有資料放進redis
   const heroes = await axios({
     method: "get",
     baseURL: process.env.HAHOWBASEURL,
@@ -15,8 +15,31 @@ const redisData = async () => {
     "Content-Type": "application/json",
     Accept: "application / json",
   });
-  //將取得的ㄘeroes資料放進redis
+  //將取得的heroes資料放進redis
   await redisClient.setAsync("heroes", JSON.stringify(heroes.data));
+
+  //2.取得單一hero的資料
+  for (let i = 1; i <= 4; i++) {
+    let hero = await axios({
+      method: "get",
+      baseURL: process.env.HAHOWBASEURL,
+      url: `/heroes/${i}`,
+      "Content-Type": "application/json",
+      Accept: "application / json",
+    });
+    //如果出現錯誤code = 1000，就繼續呼叫該api，取到值為止
+    while (hero.data["code"]) {
+      hero = await axios({
+        method: "get",
+        baseURL: process.env.HAHOWBASEURL,
+        url: `/heroes/${i}`,
+        "Content-Type": "application/json",
+        Accept: "application / json",
+      });
+    }
+    //每一筆hero資料都是一筆redis資料
+    await redisClient.setAsync(`heroes/${i}`, JSON.stringify(hero.data));
+  }
 };
 
 //server一啟動，自動串連hero api，將hero資料放進redis方便快速拿取
