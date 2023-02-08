4bd4ac502e81dc2061111c056571f7df0d58f619
Emily81926 <emily81926@gmail.com>
2023-02-08 12:08:22 +0800

    feat: add put singlehero profile in redis feature in redisData.js

--- models/redisData.js
+++ models/redisData.js
@@ -17,8 +17,9 @@ const redisData = async () => {
   });
   //將取得的heroes資料放進redis
   await redisClient.setAsync("heroes", JSON.stringify(heroes.data));
+  console.log("heroes資料已放進redis")
 
-  //2.取得單一hero的資料
+  //2.將每筆hero資料分別放入redis
   for (let i = 1; i <= 4; i++) {
     let hero = await axios({
       method: "get",
@@ -40,10 +41,32 @@ const redisData = async () => {
     //每一筆hero資料都是一筆redis資料
     await redisClient.setAsync(`heroes/${i}`, JSON.stringify(hero.data));
   }
+
+  console.log("single hero資料已放進redis");
+  //3.將每筆hero含profile資料放進redis
+  for (let i = 1; i <= 4; i++) {
+    const profile = await axios({
+      method: "get",
+      baseURL: process.env.HAHOWBASEURL,
+      url: `/heroes/${i}/profile`,
+      "Content-Type": "application/json",
+      Accept: "application / json",
+    });
+
+    const hero = await redisClient.getAsync(`heroes/${i}`);
+    const heroParse = JSON.parse(hero);
+    //將profile資料放進hero資料
+    heroParse["profile"] = profile.data;
+
+    await redisClient.setAsync(
+      `heroes/${i}/profiles`,
+      JSON.stringify(heroParse)
+    );
+  }
+  console.log("single hero profile 資料已放進redis");
 };
 
 //server一啟動，自動串連hero api，將hero資料放進redis方便快速拿取
 redisData();
 
 module.exports = redisData;
-
