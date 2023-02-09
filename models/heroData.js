32957d05596391748c0304ea6ef8fe13a3bb8b42
Emily81926 <emily81926@gmail.com>
2023-02-09 13:10:01 +0800

    refactor: add setRedis function in heroData.js

--- models/heroData.js
+++ models/heroData.js
@@ -6,64 +6,49 @@ const redisClient = Redis.createClient();
 const DEFAULT_EXPIRATION = 3600;
 
 const heroData = {
-  //取得所有heroes的資料
+  //取得未驗證heroes的資料
   getAllHeroes: async (req, res, callback) => {
-    //先查看Redis內部是否有檔案
+    //先查看Redis內部是否有檔案，如果有資料則從redis取資料
     redisClient.get("heroes", async (error, heroes) => {
       if (error) console.error(error);
-      //如果redis有資料，就從redis取資料
       if (heroes != null) {
         return callback(JSON.parse(heroes));
       } else {
         //如果沒有，則axios取得資料後，存入redis
         const heroes = await getAxiosRequest("/heroes");
-        //將取得的資料放進Redis，縮短取用資料的時間
-        redisClient.setex(
-          "heroes",
-          DEFAULT_EXPIRATION,
-          JSON.stringify(heroes.data)
-        );
+        await setRedis("heroes", heroes.data);
         return callback(heroes.data);
       }
     });
   },
 
-  //取得單一hero的資料
+  //取得未驗證單一hero的資料
   getSingleHero: async (req, res, callback) => {
     const heroId = req.params["heroId"];
     redisClient.get(`heroes/${heroId}`, async (error, hero) => {
       if (error) console.error(error);
-      //如果redis有資料，就從redis取資料
       if (hero != null) {
         return callback(JSON.parse(hero));
       } else {
-        //呼叫hero api
         let hero = await getAxiosRequest(`/heroes/${heroId}`);
-        //如果出現錯誤code = 1000，就繼續呼叫該api，取到值為止
+        //呼叫hero api，如果出現錯誤code = 1000，就繼續呼叫該api，取到值為止
         while (hero.data["code"]) {
           hero = await getAxiosRequest(`/heroes/${heroId}`);
         }
-        //將取得的資料放進Redis，縮短取用資料的時間
-        redisClient.setex(
-          `heroes/${heroId}`,
-          DEFAULT_EXPIRATION,
-          JSON.stringify(hero.data)
-        );
+        await setRedis(`heroes/${heroId}`, hero.data);
         return callback(hero.data);
       }
     });
   },
 
-  //取得單一hero profile資料
+  //取得成功驗證單一hero profile資料
   getSingleProfile: async (req, res, callback) => {
     const heroId = req.params["heroId"];
     redisClient.get(`heroes/${heroId}/profiles`, async (error, profile) => {
       if (error) console.error(error);
-      //如果redis有資料，就從redis取資料
       if (profile != null) {
         return callback(JSON.parse(profile));
       } else {
-        //如果redis沒有資料就呼叫api，並將資料放進redis
         let hero = await getAxiosRequest(`/heroes/${heroId}`);
         //如果出現錯誤code = 1000，就繼續呼叫該api，取到值為止
         while (hero.data["code"]) {
@@ -72,14 +57,9 @@ const heroData = {
 
         //取得該id的profile資料
         const profile = await getAxiosRequest(`/heroes/${heroId}/profile`);
-        //將兩個資料做合併
+        //取得hero跟profile並將兩個資料做合併
         hero.data["profile"] = profile.data;
-        //資料放入redis
-        redisClient.setex(
-          `heroes/${heroId}/profiles`,
-          DEFAULT_EXPIRATION,
-          JSON.stringify(hero.data)
-        );
+        await setRedis(`heroes/${heroId}/profiles`, hero.data);
         return callback(hero.data);
       }
     });
@@ -89,7 +69,6 @@ const heroData = {
   getHeroesProfiles: async (req, res, callback) => {
     redisClient.get("heroes/profiles", async (error, heroes) => {
       if (error) console.error(error);
-      //如果redis有資料，就從redis取資料
       if (heroes != null) {
         return callback(JSON.parse(heroes));
       } else {
@@ -98,7 +77,6 @@ const heroData = {
         for (let i = 1; i <= 4; i++) {
           //取得該id的hero資料
           let hero = await getAxiosRequest(`/heroes/${i}`);
-
           while (hero.data["code"]) {
             hero = await getAxiosRequest(`/heroes/${i}`);
           }
@@ -109,16 +87,20 @@ const heroData = {
           //將data放入authHeroes array中
           authHeroes.push(hero.data);
         }
-        //將heroes profiles放入redis方便快速拿取
-        await redisClient.setex(
-          "heroes/profiles",
-          DEFAULT_EXPIRATION,
-          JSON.stringify(authHeroes)
-        );
+        await setRedis("heroes/profiles", authHeroes);
         return callback(authHeroes);
       }
     });
   },
 };
+
+//將data放進redis的function
+function setRedis(key, data){
+  return redisClient.setex(
+    key,
+    DEFAULT_EXPIRATION,
+    JSON.stringify(data)
+  );
+}
 
 module.exports = heroData;
